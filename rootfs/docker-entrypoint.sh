e2c6beec65e092eb89ee01cb1a01fcb4f8074cf1
Axel Rindle <mail@axelrindle.de>
2025-12-18 08:53:30 +0100

    feat: custom script runner
    makes the script fail if a hook fails

--- rootfs/docker-entrypoint.sh
+++ rootfs/docker-entrypoint.sh
@@ -3,13 +3,24 @@
 set -ex
 
 TEMP_CERTS="/tmp/certs"
+HOOKS_DIR="/etc/ca-certificates/update.d"
 
 mkdir -p "${TEMP_CERTS}"
 
 update-ca-certificates --fresh --verbose \
-    --etccertsdir "${TEMP_CERTS}"
+    --etccertsdir "${TEMP_CERTS}" \
+    --hooksdir "/dev/null"
 
 mkdir -p "${CA_OUTPUT_DIR}"
 
 find "${TEMP_CERTS}" -type f -exec cp -Lpf {} "${CA_OUTPUT_DIR}" \;
 find "${TEMP_CERTS}" -type l -exec cp -Lpf {} "${CA_OUTPUT_DIR}" \;
+
+if [ -d "$HOOKS_DIR" ]
+then
+  echo "Running hooks in $HOOKS_DIR..."
+
+  run-parts --verbose --exit-on-error "$HOOKS_DIR"
+
+  echo "done."
+fi
